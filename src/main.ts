bc5c5725789ce9bede71ad2c86416b115ada9514
Yusuf Fachroni <yusuf.fachroni@gmail.com>
2024-07-23 10:39:43 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import * as serverless from 'serverless-http';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // await app.listen(3000);
+  app.enableCors();
+  await app.init();
 }
 bootstrap();
+
+module.exports.handler = serverless(server);
